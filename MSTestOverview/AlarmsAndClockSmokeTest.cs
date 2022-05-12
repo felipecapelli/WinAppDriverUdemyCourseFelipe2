86e5eedbbf079119f49ca1670b41f492ed58cb64
felipecapelli <felipecapelli_uru@hotmail.com>
2022-05-11 22:31:54 -0300

    Lecture 4

---
+++ MSTestOverview/AlarmsAndClockSmokeTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Diagnostics;
+
+namespace MSTestOverview
+{
+    [TestClass]
+    public class AlarmsAndClockSmokeTest
+    {
+        /*
+         ***Ordem de execução:***
+         
+            [ClassInitialize]
+                [TestInitialize]
+                    [TestMethod]
+                [TestCleanup]
+            [ClassCleanup]
+        */
+
+
+        static WindowsDriver<WindowsElement> sessionAlarms;
+
+        [ClassInitialize]
+        public static void PrepareForTestingAlarms(TestContext testContext)
+        {
+            Debug.WriteLine("Hello ClassInitialize");
+            WindowsDriver<WindowsElement> sessionAlarms;
+            AppiumOptions capabilities = new AppiumOptions();
+            capabilities.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            sessionAlarms = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), capabilities);
+        }
+
+        [ClassCleanup]
+        public static void CleanupAfterAllAlarmsTests()
+        {
+            Debug.WriteLine("Hello ClassCleanup");
+            if (sessionAlarms != null)
+            {
+                sessionAlarms.Quit();
+            }
+        }
+
+        [TestInitialize]
+        public static void BeforeATest()
+        {
+            Debug.WriteLine("Before a test, calling TestInitialize");
+        }
+
+        [TestCleanup]
+        public static void AfterAtest()
+        {
+            Debug.WriteLine("After a test, calling TestCleanup");
+        }
+
+        [TestMethod]
+        public static void JustAnotherTest()
+        {
+            Debug.WriteLine("Hello another test.");
+        }
+
+        [TestMethod]
+        public void TestAlarmsAndClockIsLaunchingSuccessfully()
+        {
+
+            Assert.AreEqual("Alarmes e Relógio2", sessionAlarms.Title, false,
+                    $"Actual title doesn't match expected title: {sessionAlarms.Title}"
+                );
+        }
+    }
+}
---
+++ MSTestOverview/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestOverview")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestOverview")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f7a48be-f69b-40dc-87ff-307660d085d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
